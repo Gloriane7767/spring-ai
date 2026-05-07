db1dcfce69b09937d8c598c9ff89613a7aab8559
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-05-07 14:18:51 +0200

    Initial commit

---
+++ src/test/java/com/gloriane/springai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gloriane.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
